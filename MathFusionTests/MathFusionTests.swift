652660a9398354b367215c476f7f3832f9318af8
Polina Luschevskaya <luschevskaya.p@gmail.com>
2025-04-14 16:35:52 +0300

    Initial Commit

---
+++ MathFusionTests/MathFusionTests.swift
@@ -0,0 +1,17 @@
+//
+//  MathFusionTests.swift
+//  MathFusionTests
+//
+//  Created by Полина Лущевская on 14.04.25.
+//
+
+import Testing
+@testable import MathFusion
+
+struct MathFusionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
